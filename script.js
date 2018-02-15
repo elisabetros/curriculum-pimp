c64c775acfbb48850e6ad6c729911e3e35100612
liyan <yanx0167@stud.kea.dk>
2018-02-15 11:56:26 +0100

    added: fold in the additional section

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let expand = document.querySelectorAll(".expand");
 let close = document.querySelectorAll(".x");
+let arrow = document.querySelectorAll('sub'); // need to change later
 
 expand.forEach(showDetail)
 
@@ -16,5 +17,13 @@ function hideDetails(x){
     x.addEventListener("click", hideIndividual);
     function hideIndividual(){
         x.parentElement.classList.add("hide");
+    }
+}
+
+arrow.forEach(clickArrow);
+function clickArrow(a){
+    a.addEventListener('click', unfold);
+    function unfold(){
+        a.parentElement.parentElement.classList.toggle('unfold');
     }
 }
